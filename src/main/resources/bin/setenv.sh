a880c63290275e3858ffd8117636dc267dddd39d
ppy233 <66505192+ppy233@users.noreply.github.com>
2021-05-17 12:10:50 +0800

    Update setenv.sh

--- src/main/resources/bin/setenv.sh
+++ src/main/resources/bin/setenv.sh
@@ -18,11 +18,12 @@ ACTIVE_PROFILE=$ARG_ENV
 JAVA_OPTS="-server -Xms${JVM_HEAP}m -Xmx${JVM_HEAP}m -Xmn${JVM_EDEN}m -XX:MetaspaceSize=${JVM_META}m -XX:MaxMetaspaceSize=${JVM_MAX_META}m -XX:SurvivorRatio=8 -Xss256k -XX:-UseAdaptiveSizePolicy -XX:+PrintPromotionFailure -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$APP_LOGS_DIR/oom.hprof -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:CMSInitiatingOccupancyFraction=70 -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:GCLogFileSize=100M -Xloggc:$APP_LOGS_DIR/gc.log"
 
 JAVA_MONITOR_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=7777 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
-
-JAVA_OPTS=" $JAVA_OPTS $JAVA_MONITOR_OPTS -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Dfile.encoding=UTF-8 -DAppBaseDir=$APP_BASE"
+JAVA_OPTS_BASE="-Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Dfile.encoding=UTF-8 -DAppBaseDir=$APP_BASE"
 
 if [[ $WCloud_Env != "Product" ]]; then
-    JAVA_OPTS=" $JAVA_OPTS -Xrunjdwp:transport=dt_socket,address=18848,server=y,suspend=n -Xdebug -Xnoagent -Djava.compiler=NONE"
+    JAVA_OPTS=" $JAVA_OPTS $JAVA_MONITOR_OPTS $JAVA_OPTS_BASE -Xrunjdwp:transport=dt_socket,address=18848,server=y,suspend=n -Xdebug -Xnoagent -Djava.compiler=NONE"
+    else
+    JAVA_OPTS=" $JAVA_OPTS $JAVA_OPTS_BASE"
 fi
 
 #if [[ "@environment@" == "dev" ]]; then
